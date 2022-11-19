2032eb8ba7a7a37425f5c7ffa6ddf1bdaf8092ee
Ruth Ivorra Albert <rutivoalb@alu.edu.gva.es>
2022-11-19 18:03:23 +0100

    Funciones de gastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,7 +2,7 @@
 
 // TODO: Variable global
 let presupuesto = 0;
-let gastos = {};
+let gastos = [];
 let idGasto = 0;
 
 //JS I
@@ -25,7 +25,7 @@ function mostrarPresupuesto() {
 
 class CrearGasto {
 
-    constructor(descripcion,valor,fecha) {
+    constructor(descripcion,valor) {
         if (valor > 0) {
             this.valor = valor;
         }
@@ -34,10 +34,15 @@ class CrearGasto {
         }
         this.descripcion = descripcion;
         this.fecha = Date.now();
-        this.actualizarFecha(fecha)
+        if (arguments.length > 2)
+        {
+            this.actualizarFecha(arguments[2])
+        }
         this.etiquetas = [];
-        this.anyadirEtiquetas(arguments.slice(3))
-        
+        if (arguments.length > 3)
+        {
+            this.anyadirEtiquetas(arguments.slice(3))
+        }   
     }
 
     mostrarGasto () {
@@ -55,7 +60,7 @@ class CrearGasto {
     }
 
     mostrarGastoCompleto () {
-        var etiquetas = ""
+        let etiquetas = "";
         this.etiquetas.forEach(element => {
             etiquetas = etiquetas + "\n- " + element;
         });
@@ -69,17 +74,17 @@ class CrearGasto {
     }
 
     anyadirEtiquetas () {
-        for (var i = 0; i<arguments.length; i++)
+        for (let i = 0; i<arguments.length; i++)
         {
             if (!this.etiquetas.includes(arguments[i]))
-                this.etiquetas.push(arguments[i])
+                this.etiquetas.push(arguments[i]);
         }
     }
 
     borrarEtiquetas () {
-        for (var i = 0; i<arguments.length; i++)
+        for (let i = 0; i<arguments.length; i++)
         {
-            var idx = this.etiquetas.indexOf(arguments[i]);
+            let idx = this.etiquetas.indexOf(arguments[i]);
             if (idx > -1) { 
                 array.splice(idx, 1);
             }
@@ -93,13 +98,27 @@ function listarGastos() {
     return gastos;
 }
 
-function anyadirGasto() {}
+function anyadirGasto(gasto) {
+    gasto.id = idGasto;
+    idGasto++;
+    gastos.push(gasto);
+}
 
-function borrarGasto() {}
+function borrarGasto(id) {
+    gastos = gastos.filter(g => g.id != id);
+}
 
-function calcularTotalGastos() {}
+function calcularTotalGastos() {
+    let suma = 0.0;
+    gastos.forEach(g => {
+        suma = suma + g.valor;
+    });
+    return suma;
+}
 
-function calcularBalance() {}
+function calcularBalance() {
+    return presupuesto - calcularTotalGastos();
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
